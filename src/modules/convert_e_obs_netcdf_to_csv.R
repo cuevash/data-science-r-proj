8445edf886b3e61e2acb259c0c0be9d2b81fe294
Hector Cuevas <cuevash.sp@gmail.com>
2022-11-30 11:30:59 +0100

    added pipeline worldcities.csv

--- src/modules/convert_e_obs_netcdf_to_csv.R
+++ src/modules/convert_e_obs_netcdf_to_csv.R
@@ -14,16 +14,14 @@ e_obs_lonlat_sf <-
   tidync::hyper_filter(time = index == 1) |>
   tidync::hyper_tibble() |> 
   # add date_time as proper datetime column from the original time column
-  dplyr::mutate(date_time = lubridate::as_datetime(c(time * 60 * 60 * 24), origin="1950-01-01")) |>
+  dplyr::mutate(date_time = lubridate::as_date(c(time), origin="1950-01-01")) # |>
   # Separate date parts to help with query processing
-  dplyr::mutate(year = lubridate::year(date_time)) |>
-  dplyr::mutate(month = lubridate::month(date_time)) |>
-  dplyr::mutate(month_name = lubridate::month(date_time, label = TRUE)) |>
-  dplyr::mutate(decade = as.character(as.integer((year %/% 10) * 10))) # Add decade class
+  # dplyr::mutate(year = lubridate::year(date_time)) |>
+  # dplyr::mutate(month = lubridate::month(date_time)) |>
+  # dplyr::mutate(month_name = lubridate::month(date_time, label = TRUE)) |>
+  # dplyr::mutate(decade = as.character(as.integer((year %/% 10) * 10))) # Add decade class
 
   readr::write_csv2(e_obs_lonlat_sf, out_file)
 }
 
-print(out_file)
-
-#convert_e_obs_netcdf_to_csv(netcdf_file, out_file)
+convert_e_obs_netcdf_to_csv(netcdf_file, out_file)
